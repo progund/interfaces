e207190eae12aeb60fc96d22afa19f44b4e67f77
Henrik Sandklef <hesa@sandklef.com>
2016-10-11 11:16:10 +0200

    Changed ArrayList to array

--- implementing-interfaces/net/sortex/books/test/BookTest.java
+++ implementing-interfaces/net/sortex/books/test/BookTest.java
@@ -9,16 +9,15 @@ import java.util.Arrays;
 public class BookTest {
 
     public static void main(String[] args) {
-        ArrayList<Book> books = new ArrayList<>();
+        Book[] books = { new Book("ABC ... and more", 1980),
+                         new Book("A book", 1980),
+                         new Book("A book", 1981),
+                         new Book("Book again", 1984),
+                         new Book("Another book", 1990)
+        };
 
-        books.add(new Book("ABC ... and more", 1980));
-        books.add(new Book("A book", 1980));
-        books.add(new Book("A book", 1981));
-        books.add(new Book("Book again", 1984));
-        books.add(new Book("Another book", 1990));
-
-        Collections.sort(books);
-        System.out.println(Arrays.toString(books.toArray()));
+        Arrays.sort(books);
+        System.out.println(Arrays.toString(books));
 
         
     }
