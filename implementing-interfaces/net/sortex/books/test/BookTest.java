da4c85a1a77d7ebe6319e898b384a4a46d7e9ae5
Henrik Sandklef <hesa@sandklef.com>
2016-10-13 23:07:49 +0200

    more books

--- implementing-interfaces/net/sortex/books/test/BookTest.java
+++ implementing-interfaces/net/sortex/books/test/BookTest.java
@@ -1,6 +1,7 @@
 package net.sortex.books.test;
 
 import net.sortex.books.Book;
+import net.sortex.books.BookYearComparator;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -11,15 +12,29 @@ public class BookTest {
     public static void main(String[] args) {
         Book[] books = { new Book("ABC ... and more", 1980),
                          new Book("A book", 1980),
-                         new Book("A book", 1981),
-                         new Book("Book again", 1984),
+                         new Book("A book-3", 1982),
+                         new Book("A book-2", 1982),
+                         new Book("Book again", 1719),
+                         new Book("Book again", 1898),
+                         new Book("Book again", 2013),
+                         new Book("Book again - 3", 1984),
+                         new Book("Book again - 2", 1985),
                          new Book("Another book", 1990)
         };
 
-        Arrays.sort(books);
-        System.out.println(Arrays.toString(books));
+        System.out.println("Unsorted");
+        System.out.println(Arrays.toString(books));        
 
-        
+        System.out.println("Sorted - using names and then year");
+        Arrays.sort(books);
+        System.out.println(Arrays.toString(books));        
+
+        System.out.println("Sorted using BookYearComparator - using year and then names");
+        Arrays.sort(books, new BookYearComparator());
+        System.out.println(Arrays.toString(books));        
+
+
+
     }
 
 }
