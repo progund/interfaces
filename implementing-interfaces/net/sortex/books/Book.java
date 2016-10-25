15f35cd73ad5df0f1c9e8a811ae9a3a71f51e01f
Henrik Sandklef <hesa@sandklef.com>
2016-10-25 14:38:10 +0200

    Fixed comment

--- implementing-interfaces/net/sortex/books/Book.java
+++ implementing-interfaces/net/sortex/books/Book.java
@@ -23,12 +23,25 @@ public class Book implements Comparable<Book> {
     }
 
     public int compareTo(Book anotherBook) {
-        // Compare the String objects using String's own compareTo
-        // this.name is a String, so it is compareTo in String we're invoking
+        // We shall now compare the names (both String objects).
+        // We do this by using using String's own compareTo
+        //
+        // this.name is a String, so we invoke compareTo on anotherBook's name
         int nameCheck = this.name.compareTo(anotherBook.name());
         if (nameCheck==0) {
             // If the names are the same, check year
             return this.year - anotherBook.year();
+            /*
+             * Perhaps the above code is easier understood if presented this way:
+             * 
+             * Get the two books' years and store them in separate variables:
+             *   int year        = this.year;
+             *   int anotherYear = anotherBook.year();
+             *
+             * Ok, we have the two year variables. The 
+             *   return year - anotherYear;
+             *
+             */
         } else {
             return nameCheck;
         }
