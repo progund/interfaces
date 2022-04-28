f4f7b547672f4f77d4ef4f7a8987df97c00f2b41
Rikard Fröberg <rikard@morus.se>
2022-04-28 21:51:09 +0200

    checkstyle

--- writing-your-own-interfaces/com/superpower/filebrowser/FBOgg.java
+++ writing-your-own-interfaces/com/superpower/filebrowser/FBOgg.java
@@ -2,16 +2,16 @@ package com.superpower.filebrowser;
 
 public class FBOgg extends FBFile implements FBPlayable {
 
-    public FBOgg(String name) {
-        super(name);
-    }
-
-    public String thumbnail() {
-        return "[ogg ]";
-    }
-
-    public void play() {
-        System.out.println("faked playing of file: " + this);
-    }
-    
+  public FBOgg(String name) {
+    super(name);
+  }
+  
+  public String thumbnail() {
+    return "[ogg ]";
+  }
+  
+  public void play() {
+    System.out.println("faked playing of file: " + this);
+  }
+  
 }
