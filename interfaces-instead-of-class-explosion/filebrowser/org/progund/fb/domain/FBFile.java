637bc45a9d69e2e2bdef707c67273effcdf86449
Rikard Fröberg <rikard@morus.se>
2022-04-28 22:07:04 +0200

    checkstyle

--- interfaces-instead-of-class-explosion/filebrowser/org/progund/fb/domain/FBFile.java
+++ interfaces-instead-of-class-explosion/filebrowser/org/progund/fb/domain/FBFile.java
@@ -6,27 +6,27 @@ import java.io.File;
  * Represents a file browser file object -
  * an object representing a file to be listed in a file browswer.
  *
- * Such a file object has a File (representing the actual file
+ * <p>Such a file object has a File (representing the actual file
  * in the filesystem) and a ThumbnailGenerator which can produce
  * a String thumbnail representation, e.g. "[mp3]".
  */
 public class FBFile {
   /**
-   * This FBFile's file object - the file it represents
+   * This FBFile's file object - the file it represents.
    */
   protected File file;
 
   /**
    * This FBFile's thumbnail generator - the object it uses to
-   * generate its thumbnail
+   * generate its thumbnail.
    */
   protected ThumbnailGenerator thumbGen;
 
   /**
-   * Constructs a new FBFile
+   * Constructs a new FBFile.
    * @param file - The File this FBFile represents
    * @param thumbGen - The ThumbnailGenerator this FBFile uses
-   * to generate its thumbnail
+   *                   to generate its thumbnail
    */
   public FBFile(File file, ThumbnailGenerator thumbGen) {
     this.file = file;    
@@ -34,7 +34,7 @@ public class FBFile {
   }
 
   /**
-   * Returns this FBFile's name as a String
+   * Returns this FBFile's name as a String.
    * @return This FBFile's name as a String
    */
   public String name() {
@@ -42,7 +42,7 @@ public class FBFile {
   }
 
   /**
-   * Returns this FBFile's thumbnail as a String
+   * Returns this FBFile's thumbnail as a String.
    * @return This FBFile's thumbnail as a String
    */
   public String thumbnail() {
@@ -53,7 +53,7 @@ public class FBFile {
    * Returns a String representation of this FBFile on the format
    * <code>[thumbnail text] [name]</code>
    *
-   * for instance:<br>
+   * <p>for instance:<br>
    * <code>[mp3] mama_im_coming_home.mp3</code>
    */
   public String toString() {
