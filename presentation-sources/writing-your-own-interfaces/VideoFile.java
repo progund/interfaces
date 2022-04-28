4a9ecf4abc7dbd899ab7cf11ee6c45dd78d59a8f
Rikard Fröberg <rikard@morus.se>
2022-04-28 21:26:38 +0200

    checkstyle

--- presentation-sources/writing-your-own-interfaces/VideoFile.java
+++ presentation-sources/writing-your-own-interfaces/VideoFile.java
@@ -1,37 +1,37 @@
-public class VideoFile extends File implements Playable{
+public class VideoFile extends File implements Playable {
 
-  public static final int TV    = 0;
+  public static final int TV = 0;
   public static final int MOVIE = 1;
   public static final int MUSIC = 2;
 
   public int type;
 
-  public VideoFile(String name, int type){
+  public VideoFile(String name, int type) {
     super(name);
-    this.type=type;
+    this.type = type;
   }
 
-  public int type(){
+  public int type() {
     return type;
   }
 
-  public void play(){
+  public void play() {
     String genre;
-    switch(type){
-    case TV:
-      genre="TV";
-      break;
-    case MOVIE:
-      genre="Movie";
-      break;
-    case MUSIC:
-      genre="Music";
-    default:
-      genre="Unknown";
+    switch (type) {
+      case TV:
+        genre = "TV";
+        break;
+      case MOVIE:
+        genre = "Movie";
+        break;
+      case MUSIC:
+        genre = "Music";
+        break;
+      default:
+        genre = "Unknown";
     }
-    System.out.println("Playing the file " + name() +
-                       " which is a " + genre +
-                       " video file");
+    System.out.println("Playing the file " + name() + " which is a "
+                       + genre + " video file");
     
   }
 }
