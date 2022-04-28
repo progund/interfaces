d909c2713812f863f53451b426a4feb1f6bc3c5c
Rikard Fröberg <rikard@morus.se>
2022-04-28 21:45:09 +0200

    checkstyle

--- implementing-interfaces/net/sortex/books/test/BookTest.java
+++ implementing-interfaces/net/sortex/books/test/BookTest.java
@@ -1,35 +1,35 @@
 /**
  * This class has the wrong name - the exercises uses TestBook as the name.
  *
- * We have created a class with that name too, in the same directory as this
+ * <p>We have created a class with that name too, in the same directory as this
  * class.
  *
- * You can ignore this class if you want.
+ * <p>You can ignore this class if you want.
  */
 
 package net.sortex.books.test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import net.sortex.books.Book;
 import net.sortex.books.BookYearComparator;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Arrays;
-import java.util.Comparator;
 
 public class BookTest {
 
   public static void main(String[] args) {
-    Book[] books = { new Book("ABC ... and more", 1980),
-                     new Book("A book", 1980),
-                     new Book("A book-3", 1982),
-                     new Book("A book-2", 1982),
-                     new Book("Book again", 1719),
-                     new Book("Book again", 1898),
-                     new Book("Book again", 2013),
-                     new Book("Book again - 3", 1984),
-                     new Book("Book again - 2", 1985),
-                     new Book("Another book", 1990)
+    Book[] books = {
+      new Book("ABC ... and more", 1980),
+      new Book("A book", 1980),
+      new Book("A book-3", 1982),
+      new Book("A book-2", 1982),
+      new Book("Book again", 1719),
+      new Book("Book again", 1898),
+      new Book("Book again", 2013),
+      new Book("Book again - 3", 1984),
+      new Book("Book again - 2", 1985),
+      new Book("Another book", 1990)
     };
 
     System.out.println("Unsorted");
@@ -47,10 +47,10 @@ public class BookTest {
     // We could write our own combined comparator using two
     // lambda expressions:
     // First year, then name:
-    Comparator<Book> nameComparator = (b1, b2) ->
-      b1.name().compareTo(b2.name());
-    Comparator<Book> yearComparator = (b1, b2) ->
-      b1.year() - b2.year();
+    Comparator<Book> nameComparator =
+        (b1, b2) -> b1.name().compareTo(b2.name());
+    Comparator<Book> yearComparator =
+        (b1, b2) -> b1.year() - b2.year();
     System.out.println("Sorting on year, then name:");
     Arrays.sort(books, yearComparator.thenComparing(nameComparator));
     System.out.println(Arrays.toString(books));
@@ -58,33 +58,35 @@ public class BookTest {
     System.out.println("Sorting on year");
     Arrays.sort(books, yearComparator);
     System.out.println(Arrays.toString(books));
-
+    
+    System.out.println("Sorting on name using nameComp1");
     Comparator<Book> nameComp1 = new BookNameComparator();
+    Arrays.sort(books, nameComp1);
+    System.out.println(Arrays.toString(books));
 
-    Comparator<Book> nameComp2 = (b1,b2) -> b1.name().compareTo(b2.name());
+    System.out.println("Sorting on name using nameComp2");
+    Comparator<Book> nameComp2 = (b1, b2) -> b1.name().compareTo(b2.name());
+    Arrays.sort(books, nameComp2);
+    System.out.println(Arrays.toString(books));
 
+    System.out.println("Sorting on name using nameComp3");
     Comparator<Book> nameComp3 = Comparator.comparing(Book::name);
+    Arrays.sort(books, nameComp3);
+    System.out.println(Arrays.toString(books));
 
+    System.out.println("Sorting on name using nameComp4");
     Comparator<Book> nameComp4 = new Comparator<Book>() {
         @Override
         public int compare(Book b1, Book b2) {
           return b1.name().compareTo(b2.name());
         }
-      };
-        
-    System.out.println("Sorting on name using nameComp1");
-    Arrays.sort(books, nameComp1);
-    System.out.println(Arrays.toString(books));
-    System.out.println("Sorting on name using nameComp2");
-    Arrays.sort(books, nameComp2);
-    System.out.println(Arrays.toString(books));
-    System.out.println("Sorting on name using nameComp3");
-    Arrays.sort(books, nameComp3);
-    System.out.println(Arrays.toString(books));
-    System.out.println("Sorting on name using nameComp4");
+      };        
     Arrays.sort(books, nameComp4);
     System.out.println(Arrays.toString(books));
-    Comparator<Book> yearThenNameComp = yearComparator.thenComparing(nameComparator);        
+
+    // Alternative:
+    Comparator<Book> yearThenNameComp =
+        yearComparator.thenComparing(nameComparator);        
   }
 
 }
