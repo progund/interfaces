099b1c044020ebad6bed708914ce7765820987a0
Rikard Fröberg <rikard@morus.se>
2022-04-28 21:52:11 +0200

    checkstyle

--- writing-your-own-interfaces/com/superpower/filebrowser/FBFile.java
+++ writing-your-own-interfaces/com/superpower/filebrowser/FBFile.java
@@ -2,24 +2,24 @@ package com.superpower.filebrowser;
 
 public class FBFile {
 
-    private String name;
-    
-    public FBFile(String name) {
-        this.name = name;
-    }
-
-    public String name() {
-        return name;
-    }
-
-    public String thumbnail() {
-        return "[file]";
-    }
-
-    // new
-    public String toString() {
-        return thumbnail() + " " + name;
-    }
-    
+  private String name;
+  
+  public FBFile(String name) {
+    this.name = name;
+  }
+  
+  public String name() {
+    return name;
+  }
+  
+  public String thumbnail() {
+    return "[file]";
+  }
+  
+  @Override
+  public String toString() {
+    return thumbnail() + " " + name;
+  }
+  
 }
 
