6c233279e23d1d24f962056416ce08859334fca5
Rikard Fröberg <rikard@morus.se>
2022-04-28 21:56:07 +0200

    checkstyle

--- writing-your-own-interfaces/com/superpower/filebrowser/main/MediaPlayer.java
+++ writing-your-own-interfaces/com/superpower/filebrowser/main/MediaPlayer.java
@@ -1,25 +1,25 @@
 package com.superpower.filebrowser.main;
 
-import java.util.List;
 
 import com.superpower.filebrowser.FBPlayable;
 import com.superpower.filebrowser.utils.FileList;
+import java.util.List;
 
 public class MediaPlayer {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        String dir = ".";
-        if ( args.length!=0) {
-            dir = args[0];
-        }
-        
-        System.out.println("Playing in " + dir + ":");
-        for (FBPlayable p : FileList.listPlayable(dir)) {
-            p.play();
-        }
-        
+    String dir = ".";
+    if (args.length != 0) {
+      dir = args[0];
     }
+        
+    System.out.println("Playing in " + dir + ":");
+    for (FBPlayable p : FileList.listPlayable(dir)) {
+      p.play();
+    }
+        
+  }
 
 }
     
