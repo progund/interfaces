70c38a85eb209ce059b127c02fc47f27d5497a17
Rikard Fröberg <rikard@morus.se>
2022-04-28 21:52:47 +0200

    checkstyle

--- writing-your-own-interfaces/com/superpower/filebrowser/FBMp3.java
+++ writing-your-own-interfaces/com/superpower/filebrowser/FBMp3.java
@@ -2,16 +2,16 @@ package com.superpower.filebrowser;
 
 public class FBMp3 extends FBFile implements FBPlayable {
 
-    public FBMp3(String name) {
-        super(name);
-    }
+  public FBMp3(String name) {
+    super(name);
+  }
 
-    public String thumbnail() {
-        return "[mp3 ]";
-    }
+  public String thumbnail() {
+    return "[mp3 ]";
+  }
 
-    public void play() {
-        System.out.println("faked playing of file: " + this);
-    }
+  public void play() {
+    System.out.println("faked playing of file: " + this);
+  }
     
 }
