15c006f2d1ab5390f539292110578600c52830a6
Rikard Fröberg <rikard@morus.se>
2022-04-28 21:29:17 +0200

    checkstyle

--- presentation-sources/writing-your-own-interfaces/TestFiles.java
+++ presentation-sources/writing-your-own-interfaces/TestFiles.java
@@ -1,21 +1,23 @@
-public class TestFiles{
-  public static void main(String[] args){
-    Playable[] files={
+public class TestFiles {
+  
+  public static void main(String[] args) {
+    Playable[] files = {
       new AudioFile("Space oddity.mp3", AudioFile.MUSIC),
       new AudioFile("Cortez the killer.ogg", AudioFile.MUSIC),
       new AudioFile("Dagens eko.mp3", AudioFile.SPEECH),
       new VideoFile("Twin peaks.mp4", VideoFile.TV),
       new VideoFile("The shining.mkv", VideoFile.MOVIE)
     };
-    for(Playable file : files){
+    for (Playable file : files) {
       playFile(file);
     }
     
   }
+
   // Stupid simulation of a media player
   // capable of playing any Playable file
   // like AudioFile and MusicFile
-  static void playFile(Playable p){
+  static void playFile(Playable p) {
     p.play();
   }
 }
