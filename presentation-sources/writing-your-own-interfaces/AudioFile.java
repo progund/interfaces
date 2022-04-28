b46998134a5563531a9f52ff47b21d7f713d75bf
Rikard Fröberg <rikard@morus.se>
2022-04-28 21:11:59 +0200

    checkstyle

--- presentation-sources/writing-your-own-interfaces/AudioFile.java
+++ presentation-sources/writing-your-own-interfaces/AudioFile.java
@@ -1,23 +1,22 @@
-public class AudioFile extends File implements Playable{
+public class AudioFile extends File implements Playable {
 
   public static final int MUSIC = 0;
-  public static final int SPEECH= 1;
+  public static final int SPEECH = 1;
 
   public int type;
 
-  public AudioFile(String name, int type){
+  public AudioFile(String name, int type) {
     super(name);
-    this.type=type;
+    this.type = type;
   }
 
-  public int type(){
+  public int type() {
     return type;
   }
 
-  public void play(){
-    System.out.println("Playing the file " + name() +
-                       " which is a " + 
-                       (type==MUSIC?"music":"speech")+
-                       " file");
+  public void play() {
+    System.out.println("Playing the file " + name() + " which is a "
+                       + (type == MUSIC ? "music" : "speech")
+                       + " file");
   }
 }
