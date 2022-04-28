8bbf0f4b7e7e515a9248f88664e4f2271ea9e225
Rikard Fröberg <rikard@morus.se>
2022-04-28 21:57:09 +0200

    checkstyle

--- writing-your-own-interfaces/com/superpower/filebrowser/main/TextMain.java
+++ writing-your-own-interfaces/com/superpower/filebrowser/main/TextMain.java
@@ -1,28 +1,28 @@
 package com.superpower.filebrowser.main;
 
-import java.util.List;
 
-import com.superpower.filebrowser.utils.FileList;
-import com.superpower.filebrowser.FBFile;
 import com.superpower.filebrowser.FBDir;
+import com.superpower.filebrowser.FBFile;
 import com.superpower.filebrowser.FBJavaFile;
+import com.superpower.filebrowser.utils.FileList;
+import java.util.List;
 
 public class TextMain {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        String dir = ".";
-        if ( args.length!=0) {
-            dir = args[0];
-        }
-        
-        List<FBFile> files = FileList.list(dir);
-
-        for (FBFile f : files) {
-            System.out.println(f.thumbnail() + " " + f.name());
-        }
-        
+    String dir = ".";
+    if (args.length != 0) {
+      dir = args[0];
     }
+        
+    List<FBFile> files = FileList.list(dir);
+
+    for (FBFile f : files) {
+      System.out.println(f.thumbnail() + " " + f.name());
+    }
+        
+  }
 
 }
     
